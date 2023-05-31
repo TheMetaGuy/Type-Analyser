3cf98ca3d51523fd000507d452d99b9f3029e437
owen.cullum <owen.cullum@metagu.com>
2023-05-31 14:36:24 +0100

    Add detection of Symbol Polyfills.  These are usually required when transpiling down to ES5

--- src/getType.js
+++ src/getType.js
@@ -7,11 +7,21 @@
 function getType(obj) {
     let typeStr = typeof(obj);
 
-    if (typeStr !== 'object' && typeStr !== 'function') {
+    if (typeStr !== 'object' && typeStr !== 'function' && typeStr !== 'string') {
         return typeStr;
     }
-    // convert an object to a string forrm of it's primitive value.
-    typeStr = Object.prototype.toString.call(obj).slice(8, -1).toLowerCase();  // slice(8, -1) removes the constant [object ] and the last ]
+
+    // special case to handle ES5 Symbol Polyfills which should always have a value of Symbol(something)
+    if ( typeStr == 'string' ) {
+        if ( obj.valueOf && obj.valueOf().toString().slice(0,6) === 'Symbol') {
+            return 'symbol';
+        }
+        else return typeStr;
+    }
+
+    // get a more detailed string representation of the object's type
+    // slice(8, -1) removes the constant '[object' and the last ']' parts of the string
+    typeStr = Object.prototype.toString.call(obj).slice(8, -1).toLowerCase();  
 
     // if it's a class
     if ( typeStr === 'object' && obj.constructor && obj.constructor.name) {
