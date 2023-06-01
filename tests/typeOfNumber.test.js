4931f54d387f57322b00f342ecb083635c916a17
owen.cullum <owen.cullum@metagu.com>
2023-06-01 01:12:12 +0100

    Add more number tests

--- tests/typeOfNumber.test.js
+++ tests/typeOfNumber.test.js
@@ -1,26 +1,61 @@
 import { typeOfNumber } from '../src/index.js';
 
-test('getNumberType of NaN', () => {
+test('get number type of NaN', () => {
   expect(typeOfNumber(NaN)).toBe('nan');
   expect(typeOfNumber('not a number')).toBe('nan');
 });
 
-test('getNumberType of Infinity', () => {
+test('get number type of Infinity', () => {
   expect(typeOfNumber(Infinity)).toBe('infinity');
   expect(typeOfNumber(-Infinity)).toBe('infinity');
 });
 
-test('getNumberType of safeinteger', () => {
+test('get number type of safeinteger', () => {
   expect(typeOfNumber(10)).toBe('safeinteger');
   expect(typeOfNumber(Number.MAX_SAFE_INTEGER)).toBe('safeinteger');
 });
 
-test('getNumberType of unsafe integer', () => {
+test('get number type of unsafe integer', () => {
   expect(typeOfNumber(Number.MAX_SAFE_INTEGER + 1)).toBe('unsafeNumber');
   expect(typeOfNumber(Number.MIN_SAFE_INTEGER - 1)).toBe('unsafeNumber');
 });
 
-test('getNumberType of float', () => {
+test('get number type of float', () => {
   expect(typeOfNumber(1.1)).toBe('float');
 });
 
+test ('get number type of bigint', () => {
+  expect(typeOfNumber(BigInt(123))).toBe('bigint');
+});
+
+test ('get number type of string', () => {
+  expect(typeOfNumber('123')).toBe('nan');
+});
+
+test ('get number type of boolean', () => {
+  expect(typeOfNumber(true)).toBe('nan');
+});
+
+test ('get number type of null', () => {
+  expect(typeOfNumber(null)).toBe('nan');
+});
+
+test ('get number type of object', () => {
+  expect(typeOfNumber({})).toBe('nan');
+});
+
+test ('get number type of Symbol', () => {  
+  expect(typeOfNumber(Symbol('hello'))).toBe('nan');
+});
+
+test ('get number type of undefined', () => {
+  expect(typeOfNumber(undefined)).toBe('nan');
+});
+
+test ('get number type of function', () => {
+  expect(typeOfNumber(function() {})).toBe('nan');
+});
+
+test ('get number type of array', () => {
+  expect(typeOfNumber([])).toBe('nan');
+});
