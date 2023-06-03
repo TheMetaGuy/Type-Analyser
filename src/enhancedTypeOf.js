00c2fdcbc6ff66bb155bdc574a5a6cceb0d9b51c
owen.cullum <owen.cullum@metagu.com>
2023-06-03 19:53:08 +0100

    Tiny performance tweaks

--- src/enhancedTypeOf.js
+++ src/enhancedTypeOf.js
@@ -7,12 +7,14 @@
 function enhancedTypeOf(obj) {
     let typeStr = typeof(obj);
 
+    if (obj === null) return 'null';
+
     if (typeStr !== 'object' && typeStr !== 'function' && typeStr !== 'string') {
         return typeStr;
     }
 
     // special case to handle ES5 Symbol Polyfills which should always have a value of Symbol(something)
-    if ( typeStr == 'string' ) {
+    if ( typeStr === 'string' ) {
         if ( obj.valueOf && obj.valueOf().toString().slice(0,6) === 'Symbol') {
             return 'symbol';
         }
@@ -22,7 +24,6 @@ function enhancedTypeOf(obj) {
     // get a more detailed string representation of the object's type
     // slice(8, -1) removes the constant '[object' and the last ']' parts of the string
     typeStr = Object.prototype.toString.call(obj).slice(8, -1).toLowerCase();  
-
     
     if ( typeStr === 'object' && obj.constructor ) {
         let es6ClassName = obj.constructor.name;
