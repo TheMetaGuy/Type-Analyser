c3ecf5357ead689e955d8e2481defc03ecc00627
owen.cullum <owen.cullum@metagu.com>
2023-06-14 23:14:22 +0100

    add minified versions to the output 'dist' Folder

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
 
 export default {
   input: 'src/index.js',
@@ -9,13 +10,29 @@ export default {
       format: 'esm'
     },
     {
+      file: 'dist/type-master.esm.min.js', 
+      format: 'esm',
+      plugins: [terser()]
+    },    
+    {
       file: 'dist/type-master.iife.js',
       format: 'iife',
       name: 'typeMaster'
     },
     {
+      file: 'dist/type-master.iife.min.js',
+      format: 'iife',
+      name: 'typeMaster',
+      plugins: [terser()]
+    },    
+    {
       file: 'dist/type-master.cjs.js',
       format: 'cjs'
+    },
+    {
+      file: 'dist/type-master.cjs.min.js',
+      format: 'cjs',
+      plugins: [terser()]
     }
   ],
   plugins: [
