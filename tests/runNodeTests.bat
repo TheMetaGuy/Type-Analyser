aa8b364f2ddb9ca393599dc912fc7f85f71c59f7
owen.cullum <owen.cullum@metagu.com>
2023-06-14 18:34:34 +0100

    Delete and copy over the src and dist files before every set of tests to avoind potential future issues as files added and removed

--- tests/runNodeTests.bat
+++ tests/runNodeTests.bat
@@ -2,6 +2,10 @@
 
 echo ==================================================
 echo ====== copying src and distribution files ========
+if exist src-copy rmdir /s /q src-copy
+if exist dist-copy rmdir /s /q dist-copy
+md src-copy
+md dist-copy
 copy ..\src\*.js src-copy\*.js
 copy ..\dist\*.js dist-copy\*.js
 
